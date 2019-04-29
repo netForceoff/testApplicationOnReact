c710cf69d7c2bb2882a598d69e2403a3e2b91e4d
Sergey Krasnoyarov <krasnoyarovsergei@gmail.com>
2019-04-29 08:04:41 +0500

    adding auth

---
+++ src/components/UI/input/input.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import './input.css'
+
+function isInvalid({valid, touched, shouldValidate}) { // Подходит по формату
+    return !valid && shouldValidate && touched // Если не валидный, должны валидировать и уже мы его потрогали, заполняли
+}
+
+const Input = props => {
+    const inputType = props.type || 'text'
+    const cls = ['Input']
+    const htmlFor = `${inputType}-${Math.random()}`
+
+    if (isInvalid(props)) {
+        cls.push('invalid')
+    }
+
+    return (
+        <div className = {cls.join(' ')}>
+            <label htmlFor = {htmlFor}>{props.label}</label>
+            <input
+            type = {inputType}
+            id = {htmlFor}
+            value = {props.value}
+            onChange = {props.onChange}
+            />
+            {
+                isInvalid(props)
+                ? <span>{props.errorMessage  || 'Введите верное значение'}</span>
+                : null
+            }
+
+            
+            </div>
+    )
+}
+
+export default Input
--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,10 +1,86 @@
 import React from 'react'
+import './Auth.css'
+import Button from '../../components/UI/Button/Button'
+import Input from '../../components/UI/input/input'
 
 export default class Auth extends React.Component {
+
+    state = {
+        formControls: { // Создание инпутов 
+            email: {
+                value: '',
+                type: 'email',
+                label: 'Email',
+                errorMessage: 'Введите корректный email',
+                valid: false,
+                touched: false,
+                validation: {
+                    required: true,
+                    email: true
+                }
+            },
+            password: {
+                value: '',
+                type: 'password',
+                label: 'Пароль',
+                errorMessage: 'Введите корректный пароль',
+                valid: false,
+                touched: false,
+                validation: {
+                    required: true,
+                    minLength: 6
+                }
+            }
+        }
+    }
+
+    loginHandler = () => {
+
+    }
+
+    registerHandler = () => {
+
+    }
+
+    submitHandler = event => {
+        event.preventDefault()
+    }
+
+    onChangeHandler = (event, controlName) => {
+        console.log(controlName, event.target.value)
+    }
+
+    renderInputs() {
+        return Object.keys(this.state.formControls).map((controlName, index) => {
+            const control = this.state.formControls[controlName]
+            return (
+                <Input 
+                    key = {controlName + index}
+                    type = {control.type}
+                    value = {control.value}
+                    valid = {control.touched}
+                    label = {control.label}
+                    shouldValidate = {!!control.validation} // !!Приводит тип к boolean
+                    errorMessage = {control.errorMessage}
+                    onChange = {event => this.onChangeHandler(event, controlName)}
+                />
+            )
+        })
+    }
+
     render() {
         return (
-            <div>
-                <h1>Auth</h1>
+            <div className = "Auth">
+                <div>
+                    <h1>Авторизация</h1>
+
+                    <form onSubmit = {this.submitHandler} className = "AuthForm">
+                       {this.renderInputs()}
+
+                        <Button type = "success" onClick = {this.loginHandler}>Войти</Button>
+                        <Button type = "primary" onClick = {this.registerHandler}>Регистрация</Button>
+                        </form>
+                </div>
                 </div>
         )
     }
